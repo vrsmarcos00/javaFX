1933d2d565aa201095b02bd338ca7016e7bf6ab2
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-08 14:19:05 -0300

    Created alert event

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -11,6 +13,7 @@ public class ViewController {
 	@FXML
 	public void onBtTestAction() {
 		System.out.println("Click teste");
+		Alerts.showAlert("Alert title", null, "Hello Teste", AlertType.ERROR);
 	}
 
 }
